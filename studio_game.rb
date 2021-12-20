ef8fcff40b9e164a6e87579e42abdabbf4b2531f
StevenZambito <zambito.steven@icloud.com>
2021-12-20 10:40:34 -0500

    Included attr_accessors in the Player close to both read and write the health and player_name.. Also created a score method to calculate the score of a player

--- studio_game.rb
+++ studio_game.rb
@@ -1,11 +1,12 @@
 class Player
+  attr_accessor :health, :player_name
   def initialize(player_name, health=100)
     @player_name = player_name.capitalize
     @health = health
   end
 
   def to_s
-    "#{@player_name} has a health of #{@health}"
+    "#{@player_name} has a health of #{@health} and a score of #{score}"
   end
 
   def w00t
@@ -18,10 +19,16 @@ class Player
     puts "#{@player_name} got blammed!"
   end
 
+  def score
+    @health + @player_name.length
+  end
+
 end
 
 player1 = Player.new("steven", 100)
 puts player1
+puts player1.health
+puts player1.player_name
 
 player1 = Player.new("dylan", 100)
 player1.blam
@@ -29,3 +36,7 @@ puts player1
 
 player1 = Player.new("newbie", 100)
 puts player1
+player1.player_name = "lawrence"
+puts player1.player_name
+
+
