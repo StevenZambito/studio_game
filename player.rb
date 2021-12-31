c801f22ae25246c78918416bd304e9d05af2571f
StevenZambito <zambito.steven@icloud.com>
2021-12-31 13:31:14 -0500

    Implemented seperate source files for classes

---
+++ player.rb
@@ -0,0 +1,26 @@
+class Player
+  attr_accessor :health, :player_name
+
+  def initialize(player_name, health=100)
+    @player_name = player_name.capitalize
+    @health = health
+  end
+
+  def to_s
+    "#{@player_name} has a health of #{@health} and a score of #{score}"
+  end
+
+  def w00t
+    @health +=15
+    puts "#{@player_name} got wooted!"
+  end
+  
+  def blam
+    @health -=15
+    puts "#{@player_name} got blammed!"
+  end
+
+  def score
+    @health + @player_name.length
+  end
+end
