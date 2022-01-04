c498fe58bede15f24ddd339f07f70c2d628c3559
StevenZambito <zambito.steven@icloud.com>
2022-01-04 11:44:29 -0500

    Refactored code and implemented the use of a game_turn module

--- game.rb
+++ game.rb
@@ -1,5 +1,6 @@
 require_relative 'player'
 require_relative 'die'
+require_relative 'game_turn'
 
 class Game
   attr_reader :title
@@ -16,16 +17,7 @@ class Game
   def play
     puts "There are #{@players.count} players in #{@title}"
     @players.each do |player|
-      die = Die.new
-      number_rolled = die.roll
-
-      if number_rolled < 3
-        player.blam
-      elsif number_rolled < 5
-        puts "#{player.player_name} skips"
-      else
-        player.w00t
-      end
+      GameTurn.take_turn(player)
     end
   end
 end
--- game_turn.rb
+++ game_turn.rb
@@ -1,3 +1,18 @@
+require_relative 'player'
+require_relative 'die'
+
+
 module GameTurn
-  
+  def self.take_turn(player)
+    die = Die.new
+    number_rolled = die.roll
+
+    if number_rolled < 3
+      player.blam
+    elsif number_rolled < 5
+      puts "#{player.player_name} skips"
+    else
+      player.w00t
+    end
+  end
 end
