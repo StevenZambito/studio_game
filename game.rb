2e6bf20a7d2018aaf3f29471b43b800b5dca2fe6
StevenZambito <zambito.steven@icloud.com>
2022-01-21 10:58:20 -0500

    Defined a load_players method in the Game class to read the lines of the csv file

--- game.rb
+++ game.rb
@@ -60,4 +60,10 @@ class Game
       puts "#{player.points} grand total points"
     end
   end
+
+  def load_playersz(file_name)
+    File.readlines(file_name).each do |line|
+      puts line
+    end
+  end
 end
