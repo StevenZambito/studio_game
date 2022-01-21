ed1e236d6c4e90ac0f57305b5d3e50d44cc9a670
StevenZambito <zambito.steven@icloud.com>
2022-01-21 11:04:08 -0500

    Removed unecessary code in stuido_game.rb

--- studio_game.rb
+++ studio_game.rb
@@ -6,9 +6,6 @@ player3 = Player.new("newbie", 100)
 
 knuckleheads = Game.new("Knuckleheads")
 load_players("players.csv")
-knuckleheads.add_player(player1)
-knuckleheads.add_player(player2)
-knuckleheads.add_player(player3)
 knuckleheads.play(3)
 knuckleheads.print_stats
 
